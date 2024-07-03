9498d5e9c2f38b43315981f676aec5bd5264ddc4
Alex Alan Nunes <alexalannunes@gmail.com>
2024-07-03 08:29:11 -0300

    remove custom font

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,11 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { BrowserRouter } from "react-router-dom";
-import "@fontsource/roboto";
 
 const theme = extendTheme({
   fonts: {
-    body: `'Roboto', sans-serif`,
-    heading: `'Roboto', sans-serif`,
+    body: `sans-serif`,
+    heading: `sans-serif`,
   },
 });
 
