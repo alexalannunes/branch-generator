ebfb4d8daf73455943f9dea0c06ec50da460182a
Alex Alan Nunes <alexalannunes@gmail.com>
2024-07-03 08:30:10 -0300

    implement form visualization in home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,79 +1,36 @@
-import { Flex, FormControl, Input, Stack } from "@chakra-ui/react";
-import { useForm, useWatch } from "react-hook-form";
-import { useReadLocalStorage } from "usehooks-ts";
-import { BranchNameResult } from "../../components";
-import { FormFieldValues } from "../forms/forms";
+import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { useFormPrefs } from "../../hooks/use-my-prefs";
+import { ButtonFormSwitcher } from "./ui/button-form-switcher";
+import { CustomForm } from "./ui/custom-form";
 import { DefaultForm } from "./ui/default-form";
+import { useReadMyForm } from "../../hooks/use-read-my-form";
 
-function CustomForm() {
-  // Add a fallback is no values found
-  // Guarded by page bellow
-  const initialStoredFields = {
-    result: "",
-    fields: [],
-  };
-
-  // move to hook
-  const storedForm =
-    useReadLocalStorage<FormFieldValues>("my-form") || initialStoredFields;
-
-  const { register, control, getValues } = useForm<Record<string, string>>({
-    defaultValues: {},
-    mode: "onChange",
-  });
-
-  useWatch({ control });
-
-  const fields = getValues();
-
-  const hasValues = Object.entries(fields).some(([_, value]) => !!value);
-
-  const branchName = Object.keys(fields).reduce((acc, key) => {
-    acc = acc.replace(`{${key}}`, fields[key]);
-    return acc;
-  }, storedForm.result);
-
-  const result = hasValues ? branchName : "";
-
-  return (
-    <Flex flex={1} alignItems={"center"} justifyContent={"center"}>
-      <Stack w={"600px"} spacing={4}>
-        {storedForm.fields.map((field) => {
-          return (
-            <FormControl key={field.key}>
-              <Input placeholder={field.label} {...register(field.key)} />
-            </FormControl>
-          );
-        })}
-
-        <BranchNameResult enableSlug={false} branch={result} />
-      </Stack>
-    </Flex>
-  );
-}
+// move to types file
+export type FormView = "default" | "custom";
 
 export function HomePage() {
-  // Guarded by page bellow
-  const initialStoredFields = {
-    result: "",
-    fields: [],
-  };
+  const { prefs } = useFormPrefs();
+  const storedForm = useReadMyForm();
 
-  // move to hook
-  const storedForm =
-    useReadLocalStorage<FormFieldValues>("my-form") || initialStoredFields;
+  const [view, setView] = useState<FormView>(() => {
+    return prefs;
+  });
 
-  /* Custom forms can appears here
-    add tabs
-  */
+  const isCustomView = view === "custom";
 
-  // BranchNameResult can be placed here with composition (future)
-  // so it can receive a slugged branch name
+  const hasCustomForm = !!storedForm.fields.length;
 
   // Great feature: use IA to generate branch without many fields
   return (
-    <Flex flex={1}>
-      {storedForm.fields.length ? <CustomForm /> : <DefaultForm />}
+    <Flex flex={1} flexDirection={"column"}>
+      {hasCustomForm && (
+        <Flex mx={"auto"} w="600px" h={"100px"} alignItems={"flex-end"}>
+          <ButtonFormSwitcher formView={view} onChange={(v) => setView(v)} />
+        </Flex>
+      )}
+
+      {isCustomView ? <CustomForm /> : <DefaultForm />}
     </Flex>
   );
 }
