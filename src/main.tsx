d27171a42381f4e050634cffb1aeb452d9e8b3f4
Alex Alan Nunes <alexalannunes@gmail.com>
2024-07-03 08:29:03 -0300

    back to strict mdoe

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AppProviders } from "./app/providers.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <AppProviders>
-    <App />
-  </AppProviders>
-
-  /* </React.StrictMode> */
+  <StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </StrictMode>
 );
