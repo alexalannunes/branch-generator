3932b03bc681ebdf1961a5818a1fee3f9485f4ce
Alex Alan Nunes <alexalannunes@gmail.com>
2024-07-04 08:50:31 -0300

    theme for custom form

--- src/pages/home/ui/custom-form.tsx
+++ src/pages/home/ui/custom-form.tsx
@@ -1,4 +1,10 @@
-import { Flex, FormControl, Input, Stack } from "@chakra-ui/react";
+import {
+  Flex,
+  FormControl,
+  Input,
+  Stack,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { useForm, useWatch } from "react-hook-form";
 import { useReadMyForm } from "../../../hooks/use-read-my-form";
 import { BranchNameResult } from "../../../components";
@@ -25,13 +31,19 @@ export function CustomForm() {
   // bug: one field has no slug
   const result = hasValues ? branchName : "";
 
+  const inputBg = useColorModeValue("white", "gray.900");
+
   return (
     <Flex mt={20} alignItems={"center"} justifyContent={"center"}>
       <Stack w={"600px"} spacing={4}>
         {storedForm.fields.map((field) => {
           return (
             <FormControl key={field.key}>
-              <Input placeholder={field.label} {...register(field.key)} />
+              <Input
+                bg={inputBg}
+                placeholder={field.label}
+                {...register(field.key)}
+              />
             </FormControl>
           );
         })}
