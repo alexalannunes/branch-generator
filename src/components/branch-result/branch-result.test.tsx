132b9e327826502415827a7990dc6e522aa33abf
Alex Alan Nunes <alexalannunes@gmail.com>
2024-07-08 12:20:32 -0300

    mock fn copy

--- src/components/branch-result/branch-result.test.tsx
+++ src/components/branch-result/branch-result.test.tsx
@@ -1,7 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { BranchNameResult } from "..";
 
-describe("test", () => {
+const writeText = vitest.fn();
+
+Object.assign(navigator, {
+  clipboard: {
+    writeText,
+  },
+});
+
+describe("BranchNameResult", () => {
   test("render branch name", () => {
     render(<BranchNameResult branch="bug report" />);
     expect(screen.getByText(/bug-report/i)).toBeInTheDocument();
@@ -10,5 +18,14 @@ describe("test", () => {
   test("disabled slug", () => {
     render(<BranchNameResult enableSlug={false} branch="bug report" />);
     expect(screen.getByText(/bug report/i)).toBeInTheDocument();
+  });
+
+  test("button can be enable", async () => {
+    render(<BranchNameResult branch="bug report" />);
+    expect(screen.getByRole("button")).toBeEnabled();
+
+    fireEvent.click(screen.getByRole("button"));
+
+    expect(writeText).toBeCalled();
   });
 });
